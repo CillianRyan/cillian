1edb8c18954bf11dfe9dae2e43f3df2cd014a426
CillianRyan <10542494@mydbs.ie>
2021-05-18 20:02:15 +0100

    Update football.py

--- football.py
+++ football.py
@@ -69,6 +69,60 @@ def topAssists():
     mimetype='application/json'
   )
   return ret #Return the data in a string format
+  
+ @app.route("/topYellows")
+def topYellows():
+  cur = conn2.cursor() #create a connection to the SQL instance
+  cur.execute('''SELECT * FROM topYellows''') # execute an SQL statment
+  rv = cur.fetchall() #Retreive all rows returend by the SQL statment
+  Results=[]
+  for row in rv: #Format the Output Results and add to return string
+    Result={}
+    Result['firstName']=row[0].replace('\n',' ')
+    Result['lastName']=row[1]
+    Result['age']=row[2]
+    Result['nationality']=row[3]
+    Result['team']=row[4]
+    Result['appearances']=row[5]
+    Result['goals']=row[6]
+    Result['assists']=row[7]
+    Result['yellowCards']=row[8]
+    Result['redCards']=row[9]
+    Results.append(Result)
+  response={'Results':Results, 'count':len(Results)}
+  ret=app.response_class(
+    response=json.dumps(response),
+    status=200,
+    mimetype='application/json'
+  )
+  return ret #Return the data in a string format
+  
+@app.route("/topReds")
+def topReds():
+  cur = conn2.cursor() #create a connection to the SQL instance
+  cur.execute('''SELECT * FROM topReds''') # execute an SQL statment
+  rv = cur.fetchall() #Retreive all rows returend by the SQL statment
+  Results=[]
+  for row in rv: #Format the Output Results and add to return string
+    Result={}
+    Result['firstName']=row[0].replace('\n',' ')
+    Result['lastName']=row[1]
+    Result['age']=row[2]
+    Result['nationality']=row[3]
+    Result['team']=row[4]
+    Result['appearances']=row[5]
+    Result['goals']=row[6]
+    Result['assists']=row[7]
+    Result['yellowCards']=row[8]
+    Result['redCards']=row[9]
+    Results.append(Result)
+  response={'Results':Results, 'count':len(Results)}
+  ret=app.response_class(
+    response=json.dumps(response),
+    status=200,
+    mimetype='application/json'
+  )
+  return ret #Return the data in a string format
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',port='8080', ssl_context=('cert.pem', 'privkey.pem')) #Run the flask app at port 8080
